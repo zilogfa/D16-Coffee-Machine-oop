5861e8268b538b51ea15406ae8877d2db6f5fc32
zilogfa <Zilogfa@live.com>
2022-05-29 20:23:47 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+### Ali Jafarbeglou - Coffee Machine Programm - OOP - D16
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+while is_on:
+    option = menu.get_items()
+    choice = input(f"What would you like ({option})?: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
+
+
+
+
+
+# menu = Menu()
+# item = MenuItem("name", "water", "milk", "coffee", "cost")
+#
+#
+# order = input(f"Select your order {menu.get_items()}: ")
+# MenuItem(order)
+
+
+
+# is_on = True
+#
+# while is_on:
+#     choice = input("​What would you like? (espresso/latte/cappuccino): ")
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         print(f"Water: {resources['water']}ml")
+#         print(f"Milk: {resources['milk']}ml")
+#         print(f"Coffee: {resources['coffee']}g")
+#         print(f"Money: ${profit}")
+#     else:
+#         drink = MENU[choice]
+#         if is_resource_sufficient(drink["ingredients"]):
+#             payment = process_coins()
+#             if is_transaction_successful(payment, drink["cost"]):
+#                 make_coffee(choice, drink["ingredients"])
